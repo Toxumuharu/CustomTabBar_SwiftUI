8ea4ed028f4f04e62e2d791e85f36f545b35f7f8
Toxumuharu <ring.o.awlright@gmail.com>
2021-01-12 22:46:49 +0900

    first commit

--- CustomTabBar_SwiftUI/ContentView.swift
+++ CustomTabBar_SwiftUI/ContentView.swift
@@ -8,10 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    init(){
+        UITabBar.appearance().barTintColor = .systemBackground
     }
+    
+    @State var selectedIndex = 0
+    @State var shouldShowModal = false
+    let tabBarImageNames = ["person", "gear", "plus.app.fill", "pencil", "lasso"]
+    
+    var body: some View {
+        VStack(spacing: 0){
+            ZStack{
+                Spacer()
+                Button(action: {
+                    shouldShowModal.toggle()
+                }, label: {
+                    Text("Fullscreen cover")
+                    })
+
+                switch selectedIndex{
+                case 0:
+                    NavigationView{
+                        ScrollView{
+                            ForEach(0..<100) { num in
+                                Text("\(num)")
+                            }
+                        }
+                            .navigationTitle("First Tab")
+                    }
+                case 1:
+                    ScrollView{
+                        Text("Test")
+                    }
+                default:
+                    NavigationView{
+                        Text("Remaining Tabs")
+                    }
+
+                }
+            }
+            Spacer()
+            Divider() // for separate the view with a line
+                .padding(.bottom, 8)
+            
+            HStack{
+                ForEach(0..<5){ num in
+                    Button(action: {
+                        if num == 2 {
+                            shouldShowModal.toggle()
+                            return
+                        }
+                        selectedIndex = num
+                    }, label: {
+                        Spacer()
+                        
+                        if num == 2 {
+                            Image(systemName: tabBarImageNames[num])
+                            .font(.system(size: 44, weight: .bold))
+                            .foregroundColor(.red)
+                        } else {
+                        Image(systemName: tabBarImageNames[num])
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+                        }
+                        Spacer()
+                    })
+                }
+            }
+        }
+    }
+    //        TabView{
+    //            Text("First")
+    //                .tabItem {
+    //                    Image(systemName: "person")
+    //                    Text("First")
+    //                }
+    //            Text("First")
+    //                .tabItem {
+    //                    Image(systemName: "gear®")
+    //                    Text("First")
+    //                }
+    //        }
+    //    }
 }
 
 struct ContentView_Previews: PreviewProvider {
